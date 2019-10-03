17d10d6188804fced63dfa13c425dace079a0a9a
JAEHO SONG <thruthesky@gmail.com>
2019-10-03 21:07:59 +0800

    git commit -am "$1" || :

--- user.action.ts
+++ user.action.ts
@@ -2,13 +2,7 @@ import { ApiUserInformation } from 'libs/philgo-api/philgo-api-interface';
 
 export class UserProfile {
   static readonly type = '[User] Profile';
-  idx: string;
-  constructor(public user: ApiUserInformation) {
-    if (user === null) {
-      return;
-    }
-    this.idx = user.idx;
-  }
+  constructor(public user: ApiUserInformation) {}
 }
 
 
--- user.state.ts
+++ user.state.ts
@@ -6,8 +6,8 @@ import { AppService } from '@src/app/app.service';
 import { tap } from 'rxjs/operators';
 
 @State<ApiUserInformation>({
-  name: 'user',         // 'user' state 라는 것을 표시
-  defaults: {} as any   // user 정보를 담는 공간
+  name: 'user',         // 'user' state.
+  defaults: {} as any   // Set user object. Not atction.
 })
 export class UserState {
 
@@ -18,7 +18,7 @@ export class UserState {
   }
 
   /**
-   * 초기화를 한다.
+   * Initialize the state with user information.
    * 여기서 StateContext 를 가지고 원하는 작업을 할 수 있다.
    * `defaults` 메타 데이터에 초기값을 변경하는 등의 작업을 할 수 있다.
    * @param ctx context
@@ -27,30 +27,26 @@ export class UserState {
    *      즉, 이전 회원로그인 정보를 가져와서 로그인 정보를 세팅하는 것이다.
    */
   ngxsOnInit(ctx: StateContext<ApiUserInformation>) {
-    ctx.setState(this.a.user());
+    ctx.setState(this.loadUserProfileAction());
   }
 
   /**
    * Saves user profile information into the state
-   * @note 회원 정보를 업데이트 해야 할 경우가 생기면 무조건 이 Action 을 dispatch 하면 된다.
+   * @note This method will be called whenever profile updates.
    * @param ctx ctx
    * @param user user
    */
   @Action(UserProfile) profile(ctx: StateContext<ApiUserInformation>, action: UserProfile) {
-    /**
-     * 참고: 전달된 값은 action 객체 전체이다.
-     * 이 action 객체 안에, action.user, action.idx 등의 값이 존재한다.
-     */
-    this.storageService.set('user', action);
-    // 참고: patchState 를 하니까, 값이 마지막으로 적용하는 값이 제대로 적용이 되지 않는다.
-    // 참고: 그래서 setState() 를 통해서 저장한다.
-    // 참고: 면밀한 분석이 필요하다.
-    ctx.setState(action as any);
+    const user: ApiUserInformation = action.user;
+    this.saveUserProfileAction(user);
+    ctx.setState(user);
+    console.log('[UserProfile] profile updated on profile action: ', user.idx, user.email, user.nickname );
   }
 
   /**
    * User login
    * @note 사용자 로그인 Action 이 dispatch 되면 필고 백엔드로 로그인을 하고 성공/실패 여부를 알린다.
+   *    When `User Login Action` is dispatched, it does `Philgo Api Login on Backend` and return the observable.
    */
   @Action(UserLogin)
   login(ctx: StateContext<ApiUserInformation>, action: UserLogin) {
@@ -59,11 +55,19 @@ export class UserState {
         tap( user => {
           // 아래와 같이 ctx 로 다시 Action 을 dispatch 할 수 있다.
           // 아래의 Action 을 통해서 회원 정보를 localStorage 에 저장한다.
+          console.log(`[UserLogin] action dispatched & Philgo login api has done with: `, user.idx, user.email, user.nickname);
           ctx.dispatch(new UserProfile(user));
         })
       );
   }
 
+
+  loadUserProfileAction() {
+    return this.storageService.get('user');
+  }
+  saveUserProfileAction(user: ApiUserInformation) {
+    this.storageService.set('user', user);
+  }
 
 }
 
